bf57a77c5bf25124a32479e98ddab82273b8cac3
IslamSalimzhanov <94981808+Somikkk@users.noreply.github.com>
2023-09-27 23:45:19 +0600

    Add files via upload

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.conditions.task4;
 
+
+
+
 public class MaxNumberPrinter {
+    public static void main(String[] args) {
+
+    }
     public void printGreatest(int first, int second, int third) {
+        int greatest;
+
+        if(first >= second && first >= third) {
+            greatest = first;
+        } else if(second >= first && second >= third) {
+            greatest = second;
+        } else {
+            greatest = third;
+        }
+
+        System.out.println(greatest);
     }
 }
